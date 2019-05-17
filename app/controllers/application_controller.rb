b030be465444d1cce26efbe8fb129a6e9b0e18f0
Tatsuhiko Kimita <nannbo@crappo.net>
2019-05-17 13:50:16 +0900

    #2 : 1.3.4 Hello, world!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    def root
+        render html: "ここが, 世界の始まり!"
+    end
+
+    def hello
+        render html: "こんにちは, 世界!"
+    end
 end
