bc24a02b18b9dcad1a7a98d1e0087b2a323d23ae
Tatsuhiko Kimita <nannbo@crappo.net>
2019-08-04 00:55:42 +0900

    READMEの内容追加と開発環境系ファイルの更新

--- README.md
+++ README.md
@@ -1,24 +1,42 @@
 # README
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
-
-Things you may want to cover:
-
 * Ruby version
+    * 2.4.5
 
 * System dependencies
+    * when `bundle install`
+        * libsqlite3-dev
+    * when `bundel exec bin/rails s`
+        * node
 
 * Configuration
+    * No info.
 
 * Database creation
 
+```SQL
+CREATE DATABASE `bookshelf` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */
+```
+
 * Database initialization
 
+```bash
+$ bundle exec bin/rails db:migrate RAILS_ENV=development
+```
+
 * How to run the test suite
+    * No info.
 
 * Services (job queues, cache servers, search engines, etc.)
+    * No info.
+
+* Run Debug
+
+```bash
+$ bundle exec bin/rails s
+```
 
 * Deployment instructions
+    * No info.
 
 * ...
--- db/schema.rb
+++ db/schema.rb
@@ -12,14 +12,23 @@
 
 ActiveRecord::Schema.define(version: 2019_05_18_014459) do
 
-  create_table "microposts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "books", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.date "published_on"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "microposts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
