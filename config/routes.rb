ccdcdc403db5648f6194c6ff381ccb566a4d4f29
Tatsuhiko Kimita <nannbo@crappo.net>
2019-08-04 20:42:57 +0900

    #5 : 3.4.4 ルーティングの設定, 演習_3

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   resources :microposts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#root'
-  get '/hoge', to: 'hoge#hello'
-  get '/hello', to: 'application#hello'
+  # root 'application#root'
+  root 'static_pages#home'
 end
